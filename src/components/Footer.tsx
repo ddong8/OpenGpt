ef89bbbf51ac2cdb1cec0c74facb7276d6b29455
ddong8 <donghaixing8@gmail.com>
2023-03-11 15:22:02 +0000

    update twitter link.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,7 @@ export function Footer() {
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
           <div className="flex gap-x-6">
             <Link
-              href="https://twitter.com/EclipsePrayer"
+              href="https://twitter.com/ssok135"
               className="group"
               aria-label="Twitter"
             >
